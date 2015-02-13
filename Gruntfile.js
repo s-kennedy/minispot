063a95a46d971974872a0260b71f95b071b72473
Sharon <sharon.peishan.kennedy@gmail.com>
2015-02-13 11:25:05 +0100

    set up bower and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        mangle: false,
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      my_target: {
+        files: {
+          'build/main.min.js': ['src/javascript/*.js', 'bower_components/bower-tinder-js/*.js']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+         'build/main.css':'src/styles/main.scss'
+        }
+      }
+    },
+    processhtml: {
+      dist: {
+        options: {
+          process: true,
+          data: {
+            title: 'Minispot',
+            message: 'This is production distribution'
+          }
+        },
+        files: {
+          'build/index.html': ['src/index.html']
+        }
+      }
+    },
+    copy: {
+      main: {
+        src: 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        dest: 'build/bootstrap.min.css',
+      }
+    },
+    'gh-pages': {
+        options: {
+            base: 'build'
+        },
+        src: ['**']
+    }
+  });
+
+  // Load the plugin tasks
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  // Custom tasks
+  grunt.registerTask('deploy', ['gh-pages']);
+  grunt.registerTask('default', ['uglify', 'sass', 'copy', 'processhtml']);
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Making a mini Spotify player
+
+It's going to be AWESOME. 
